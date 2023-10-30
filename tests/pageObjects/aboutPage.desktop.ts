8dcb45b159de42ea56a7391bc8bc25e68dafca86
Tomas Jelinek <jeldicek@seznam.cz>
2023-10-30 22:03:03 +0100

    format

--- tests/pageObjects/aboutPage.desktop.ts
+++ tests/pageObjects/aboutPage.desktop.ts
@@ -1,7 +1,5 @@
 import { BasePage } from "./basePage.desktop";
 
 export class AboutPage extends BasePage {
-
     public readonly LocatorSignInButton = this.page.locator('[data-action = "sign in"]');
-
 }
